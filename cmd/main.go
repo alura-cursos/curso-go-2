b4da74d641dba7ae8e830df3002c2fdac3959ce3
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-19 19:44:11 -0300

    4.2

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,6 @@ func main() {
 	router.GET("/pizzas/:id", handler.GetPizzasByID)
 	router.DELETE("/pizzas/:id", handler.DeletePizzaById)
 	router.PUT("/pizzas/:id", handler.UpdatePizzaByID)
-	router.POST("pizzas/:id/reviews")
+	router.POST("pizzas/:id/reviews", handler.PostReview)
 	router.Run()
 }
