07fdc70d586db1f8e058741f4a419f1e62cf1f82
guilhermelima <guilhermelimaruby@gmail.com>
2024-07-19 15:29:34 -0300

    2.3

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"pizzaria/internal/models"
-	"strconv"
-
 	"pizzaria/internal/data"
+
+	"pizzaria/internal/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,94 +11,10 @@ import (
 func main() {
 	data.LoadPizzas()
 	router := gin.Default()
-	router.GET("/pizzas", getPizzas)
-	router.POST("/pizzas", postPizzas)
-	router.GET("/pizzas/:id", getPizzasByID)
-	router.DELETE("/pizzas/:id", deletePizzaById)
-	router.PUT("/pizzas/:id", updatePizzaByID)
+	router.GET("/pizzas", handler.GetPizzas)
+	router.POST("/pizzas", handler.PostPizzas)
+	router.GET("/pizzas/:id", handler.GetPizzasByID)
+	router.DELETE("/pizzas/:id", handler.DeletePizzaById)
+	router.PUT("/pizzas/:id", handler.UpdatePizzaByID)
 	router.Run()
-}
-
-func getPizzas(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"pizzas": pizzas,
-	})
-}
-
-func postPizzas(c *gin.Context) {
-	var newPizza models.Pizza
-	if err := c.ShouldBindJSON(&newPizza); err != nil {
-		c.JSON(400, gin.H{
-			"erro": err.Error()})
-		return
-	}
-	newPizza.ID = len(pizzas) + 1
-	pizzas = append(pizzas, newPizza)
-	savePizza()
-	c.JSON(201, newPizza)
-}
-
-func getPizzasByID(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"erro": err.Error()})
-		return
-	}
-	for _, p := range pizzas {
-		if p.ID == id {
-			c.JSON(200, p)
-			return
-		}
-	}
-	c.JSON(404, gin.H{"message": "Pizza not found"})
-}
-
-func deletePizzaById(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"erro": err.Error()})
-		return
-	}
-
-	for i, p := range pizzas {
-		if p.ID == id {
-			pizzas = append(pizzas[:i], pizzas[i+1:]...)
-			savePizza()
-			c.JSON(200, gin.H{"message": "pizza deleted"})
-			return
-		}
-	}
-	c.JSON(404, gin.H{"message": "pizza not found"})
-}
-
-func updatePizzaByID(c *gin.Context) {
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"erro": err.Error()})
-		return
-	}
-
-	var updatedPizza models.Pizza
-	if err := c.ShouldBindJSON(&updatedPizza); err != nil {
-		c.JSON(400, gin.H{"erro": err.Error()})
-		return
-	}
-
-	for i, p := range pizzas {
-		if p.ID == id {
-			pizzas[i] = updatedPizza
-			pizzas[i].ID = id
-			savePizza()
-			c.JSON(200, pizzas[i])
-			return
-		}
-	}
-
-	c.JSON(404, gin.H{"method": "pizza not found"})
 }
